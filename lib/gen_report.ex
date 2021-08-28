78d0f9e1adae4c240fceb63750fdb84c7753fdec
Lucas Frantz <maxkronus@gmail.com>
2021-08-28 08:08:28 -0300

    gerando o relatorio

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -32,9 +32,32 @@ defmodule GenReport do
   def build(filename) do
     filename
     |> Parser.parse_file()
+    |> Enum.reduce(report_template(), fn line, report -> sum_values(line, report) end)
   end
 
-  def report_template do
+  def build(), do: {:error, "Insira o nome de um arquivo"}
+
+  defp sum_values([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    hours_per_month = %{
+      hours_per_month
+      | name => Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
+    }
+
+    hours_per_year = %{
+      hours_per_year
+      | name => Map.put(hours_per_year[name], year, hours_per_year[name][year] + hours)
+    }
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp report_template do
     all_hours = Enum.into(@names, %{}, &{&1, 0})
     months = Enum.into(@months, %{}, &{&1, 0})
     years = Enum.into(2016..2020, %{}, &{&1, 0})
@@ -43,6 +66,10 @@ defmodule GenReport do
 
     hours_per_year = Enum.into(@names, %{}, fn name -> {name, years} end)
 
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
     %{
       "all_hours" => all_hours,
       "hours_per_month" => hours_per_month,
