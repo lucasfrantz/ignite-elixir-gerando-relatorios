cba7fd72c03cd4e35aa80dca2c901a68961899e2
Lucas Frantz <maxkronus@gmail.com>
2021-08-28 07:54:40 -0300

    criar report template

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,52 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @names [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
+
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+  end
+
+  def report_template do
+    all_hours = Enum.into(@names, %{}, &{&1, 0})
+    months = Enum.into(@months, %{}, &{&1, 0})
+    years = Enum.into(2016..2020, %{}, &{&1, 0})
+
+    hours_per_month = Enum.into(@names, %{}, fn name -> {name, months} end)
+
+    hours_per_year = Enum.into(@names, %{}, fn name -> {name, years} end)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
